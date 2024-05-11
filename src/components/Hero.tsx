3808cf9bc745a8767c1c3efbc8b7a4bd69e2abb4
Nicolas Perez <nicolas_perez@brown.edu>
2024-05-11 13:21:00 -0400

    feat: Created Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
 // Hero.tsx
-import { CalendarCheck } from '@phosphor-icons/react';
-import React from 'react';
-import CustomLink from './CustomLink';
+import { CalendarCheck } from "@phosphor-icons/react";
+import React from "react";
+import CustomLink from "./CustomLink";
+
+import { getImageUrl } from "@/utils/utils";
 
 interface HeroProps {
   title: string;
@@ -10,27 +12,28 @@ interface HeroProps {
   imageDesktop: string;
 }
 
-const Hero: React.FC<HeroProps> = ({title, subtitle, imageMobile, imageDesktop}) => {
-  console.log(imageMobile, imageDesktop);
+const Hero: React.FC<HeroProps> = ({
+  title,
+  subtitle,
+  imageMobile,
+  imageDesktop,
+}) => {
   return (
-    <section className="w-full h-full snap-start snap-always shrink-0 bg-teal-200">
-      {/* <div className="absolute inset-0 bg-black opacity-50"></div> */}
-      <picture className="w-full h-full">
-        <source className="object-cover" media="(min-width: 768px)" srcSet={`/cs1300-v2-portfolio/assets/${imageDesktop}`}/>
-        <img className="object-cover" src={`/cs1300-v2-portfolio/assets/${imageMobile}`} alt="IfItDoesntMatchAnyMedia"/>
+    <section className="h-[calc(100vh-80px)] min-h-screen w-full shrink-0 snap-start snap-always bg-teal-200">
+      <picture className="relative h-full w-full object-cover">
+        <source media="(min-width: 768px)" srcSet={getImageUrl(imageDesktop)} />
+        <img src={getImageUrl(imageMobile)} alt="IfItDoesntMatchAnyMedia" />
       </picture>
-      <div className="absolute top-1/3 left-1/3 transform -translate-x-1/3 -translate-y-1/3">
-        <h1 className="text-5xl md:text-8xl font-display font-bold mb-4">
+      <div className="absolute left-1/3 top-1/3 -translate-x-1/3 -translate-y-1/3 transform">
+        <h1 className="mb-4 font-display text-5xl font-bold md:text-8xl">
           {title}
         </h1>
-        <p className="text-2xl md:text-3xl mb-8">
-          {subtitle}
-        </p>
+        <p className="mb-8 text-2xl md:text-3xl">{subtitle}</p>
         <CustomLink
           to="https://calendly.com/nico_perez/30min"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex bg-yellow-200 hover:bg-yellow-300 text-primary font-display px-8 py-4 rounded-full border-2 border-black hover:shadow-neo text-lg md:text-xl tracking-wide text-center"
+          className="inline-flex rounded-full border-2 border-black bg-yellow-200 px-8 py-4 text-center font-display text-lg tracking-wide text-primary hover:bg-yellow-300 hover:shadow-neo md:text-xl"
         >
           <CalendarCheck size={24} weight="fill" className="mr-2" />
           Get in Touch
