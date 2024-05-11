c4046caa213efde62159304f5dbaa942c15746dc
Nicolas Perez <nicolas_perez@brown.edu>
2024-05-11 13:24:33 -0400

    Created Section component to simplify making sections of the pages

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,27 +1,48 @@
 import { ReactNode } from "react";
-import ImagePreview from "./ImagePreview"
+import ImagePreview from "./ImagePreview";
 
 interface SectionProps {
-    title: string;
-    reverse?: boolean;
-    children: ReactNode;
-    image?: string;
-    alt?: string;
+  title: string;
+  reverse?: boolean;
+  children: ReactNode;
+  image?: string;
+  alt?: string;
 }
 
-const Section: React.FC<SectionProps> = ({title, reverse = false, image,
-    alt, children}) => {
-    return (
-            <div className={`flex flex-col ${reverse ? 'md:flex-row-reverse' :'md:flex-row'} items-center mb-12`}>
-                <div className={`flex-1 ${reverse ? 'md:ml-12' :'md:mr-12'} mb-12 mb-0`}>
-                    <h3 className="text-4xl font-display font-bold mb-8">{title}</h3>
-                    {children}
-                </div>
-                <div className="flex-1 w-full h-full max-h-[600px]">
-                    <ImagePreview src={`/cs1300-v2-portfolio/assets/${image}`} alt={alt} />
-                </div>
-            </div>
-        );
-}
+// Slot component
+// const CardHeader = (props) => { /* ...*/ }
+// function CardFooter = (props) => { /* ... */ }
+// function Card = (props) => { /* ... */ }
+
+// Card.Header = CardHeader;
+// Card.Footer = CardFooter;
+
+// <Card>
+//   <Card.Header>...</Card.Header>
+//   <p>Content</p>
+//   <Card.Footer>...</Card.Footer>
+// </Card>
+
+const Section: React.FC<SectionProps> = ({
+  title,
+  reverse = false,
+  image,
+  alt,
+  children,
+}) => {
+  return (
+    <div
+      className={`flex flex-col ${reverse ? "md:flex-row-reverse" : "md:flex-row"} mb-12 items-center`}
+    >
+      <div className={`flex-1 ${reverse ? "md:ml-12" : "md:mr-12"} mb-0 mb-12`}>
+        <h3 className="mb-8 font-display text-4xl font-bold">{title}</h3>
+        {children}
+      </div>
+      <div className="h-full max-h-[600px] w-full flex-1">
+        <ImagePreview src={image} alt={alt} />
+      </div>
+    </div>
+  );
+};
 
 export default Section;
