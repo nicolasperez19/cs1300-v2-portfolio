ab033a9b00fd55f9e2f0f0e7460106f431947e98
Nicolas Perez <nicolas_perez@brown.edu>
2024-05-11 13:20:07 -0400

    feat: Created CustomLink component so that it ensures correct routing for internal or external links

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -2,19 +2,37 @@ import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
 interface CustomLinkProps {
-    className?: string;
-    to: string;
-    target?: string;
-    rel?: string;
-    children: ReactNode;
-    onClick?: () => void;
+  className?: string;
+  to: string;
+  target?: string;
+  rel?: string;
+  children: ReactNode;
+  onClick?: () => void;
 }
 
-const CustomLink: React.FC<CustomLinkProps> = ({className = "text-blue-400 hover:underline hover:underline-offset-8", to, children, target, rel, onClick}) => {
-    return (
-        to.match(/^(https?:)?\/\//) ? <a className={className} href={to} target={target} rel={rel} onClick={onClick}>{children}</a> :
-        <Link className={className} to={to} onClick={onClick}>{children}</Link>
-    );
-}
+const CustomLink: React.FC<CustomLinkProps> = ({
+  className = "underline font-bold",
+  to,
+  children,
+  target = "_blank",
+  rel = "noopener noreferrer",
+  onClick,
+}) => {
+  return to.match(/^(https?:)?\/\//) ? (
+    <a
+      className={className}
+      href={to}
+      target={target}
+      rel={rel}
+      onClick={onClick}
+    >
+      {children}
+    </a>
+  ) : (
+    <Link className={className} to={to} onClick={onClick}>
+      {children}
+    </Link>
+  );
+};
 
 export default CustomLink;
