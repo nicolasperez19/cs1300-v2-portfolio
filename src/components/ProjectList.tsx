dbceebdd91cdd9dd17ef849ea0a15ac97dee2985
Nicolas Perez <nicolas_perez@brown.edu>
2024-05-11 13:23:38 -0400

    chore: Updated alias link for project map

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import ProjectCard from './ProjectCard';
-import projectsMap from '../projectMap';
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import projectsMap from "@/projectMap";
 
 const ProjectList: React.FC = () => {
   return (
-    <section className="bg-yellow-200 w-full md:h-screen lg:h-full">
-      <div className="px-5 md:px-24 2xl:p-32 h-full py-20">
-        <h2 className="text-4xl font-display font-bold mb-8">Projects</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {projectsMap.map(project=> (
+    <section className="w-full bg-yellow-200 md:h-screen lg:h-full">
+      <div className="h-full px-5 py-20 md:px-24 2xl:p-32">
+        <h2 className="mb-8 font-display text-4xl font-bold">Projects</h2>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {projectsMap.map((project) => (
             <ProjectCard
-            key={project.attributes.slug}
-            title={project.attributes.title}
-            organization={project.attributes.organization}
-            description={project.attributes.description}
-            slug={project.attributes.slug}
-            image={project.attributes.imagePreview}
-            isAwardWinning={project.attributes.isAwardWinning}
-            tag={project.attributes.tag}
-          />
+              key={project.attributes.slug}
+              title={project.attributes.title}
+              organization={project.attributes.organization}
+              description={project.attributes.description}
+              slug={project.attributes.slug}
+              image={project.attributes.imagePreview}
+              isAwardWinning={project.attributes.isAwardWinning}
+              tag={project.attributes.tag}
+            />
           ))}
         </div>
       </div>
