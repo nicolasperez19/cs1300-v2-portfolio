d8814af3c7249d5ceb276ead4edbde60e70fde48
Nicolas Perez <nicolas_perez@brown.edu>
2024-05-11 13:20:20 -0400

    feat: Created AboutMe component

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import Section from './Section';
-import { CalendarCheck } from '@phosphor-icons/react';
-import CustomLink from './CustomLink';
+import React from "react";
+import Section from "./Section";
+import { CalendarCheck } from "@phosphor-icons/react";
+import CustomLink from "./CustomLink";
 
 const AboutMe: React.FC = () => {
   return (
-    <section className="bg-lime-100 w-full md:h-screen lg:h-full">
-      <div className="px-5 md:px-24 2xl:p-32 h-full py-20">
-        <Section title="About Me" image="images/home/about_me.png">
+    <section className="w-full bg-lime-100 md:h-screen lg:h-full">
+      <div className="h-full px-5 py-20 md:px-24 2xl:p-32">
+        <Section title="About Me" image="home/about_me.webp">
           <p className="mb-4">
-            I'm a senior at Brown University studying Computer Science, focusing on software and design.
+            I'm a senior at Brown University studying Computer Science, focusing
+            on software and design.
           </p>
-          <p className="mb-4">My passion lies with frontend development, web/mobile applications, and UI/UX development.</p>
-          <p className="mb-4">In the past I've built a Congressional award-winning soccer website, a mobile pharmacy app in West Africa.</p>
-          <p className="mb-4">Lately I've been tinkering with AI and machine learning.</p>
+          <p className="mb-4">
+            My passion lies with frontend development, web/mobile applications,
+            and UI/UX development.
+          </p>
+          <p className="mb-4">
+            In the past I've built a Congressional award-winning soccer website,
+            a mobile pharmacy app in West Africa.
+          </p>
+          <p className="mb-4">
+            Lately I've been tinkering with AI and machine learning.
+          </p>
           <p>Outside of coding, you'll catch me:</p>
-          <ul className="list-disc pl-6 mb-4">
+          <ul className="mb-4 list-image-square pl-6">
             <li>playing soccer</li>
             <li>drawing palm trees outside in the park</li>
             <li>dancing salsa</li>
@@ -25,7 +34,7 @@ const AboutMe: React.FC = () => {
             to="https://calendly.com/nico_perez/30min"
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex bg-yellow-200 hover:bg-yellow-300 text-primary font-display px-8 py-4 rounded-full border-2 border-black hover:shadow-neo text-lg md:text-xl trext-center"
+            className="trext-center inline-flex rounded-full border-2 border-black bg-yellow-200 px-8 py-4 font-display text-lg text-primary hover:bg-yellow-300 hover:shadow-neo md:text-xl"
           >
             <CalendarCheck size={24} weight="fill" className="mr-2" />
             Get in Touch
