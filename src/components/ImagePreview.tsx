49722a24108fb2d5430217e7b61cdb3cd8d7659b
Nicolas Perez <nicolas_perez@brown.edu>
2024-05-11 13:21:34 -0400

    fix: Fixed ImagePreview component so that it gets the correct image url for both dev and prod

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { getImageUrl } from "@/utils/utils";
 
 interface ImagePreviewProps {
   src?: string;
   alt?: string;
 }
 
-const ImagePreview: React.FC<ImagePreviewProps> = ({ src = "https://placehold.co/1920x1080", alt = "This is a placeholder" }) => {
+const ImagePreview: React.FC<ImagePreviewProps> = ({
+  src = "https://placehold.co/1920x1080",
+  alt = "This is a placeholder",
+}) => {
   const [isFullscreen, setIsFullscreen] = useState(false);
 
   const handleClick = () => {
@@ -13,19 +17,25 @@ const ImagePreview: React.FC<ImagePreviewProps> = ({ src = "https://placehold.co
   };
 
   return (
-    <div className="relative w-full h-full">
+    <div className="relative aspect-video">
       <img
-        src={src}
+        src={getImageUrl(src)}
         alt={alt}
-        className="cursor-pointer border-2 border-black rounded-md hover:shadow-neo aspect-video"
+        className="h-full w-full cursor-pointer rounded-md border-2 border-black object-cover hover:shadow-neo"
+        loading="lazy"
         onClick={handleClick}
       />
       {isFullscreen && (
         <div
-          className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-75 z-50"
+          className="fixed bottom-0 left-0 right-0 top-0 z-50 flex cursor-pointer items-center justify-center bg-black bg-opacity-75"
           onClick={handleClick}
         >
-          <img src={src} alt={alt} className="max-h-full max-w-full" />
+          <img
+            src={getImageUrl(src)}
+            alt={alt}
+            className="max-h-full max-w-full"
+            loading="lazy"
+          />
         </div>
       )}
     </div>
