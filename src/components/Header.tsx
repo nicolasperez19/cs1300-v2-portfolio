f7be1e38eba3c8da04aa28f4371f15ae6cadb8de
Nicolas Perez <nicolas_perez@brown.edu>
2024-05-11 13:20:48 -0400

    feat: Created Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,25 @@
-import { List, X } from '@phosphor-icons/react';
-import React, { useState } from 'react';
-import Logo from './Logo';
-import CustomLink from './CustomLink';
+import { List, X } from "@phosphor-icons/react";
+import React, { useState } from "react";
+import Logo from "./Logo";
+import CustomLink from "./CustomLink";
+
+const links = [
+  {
+    id: 1,
+    name: "Projects",
+    url: "/cs1300-v2-portfolio/projects",
+  },
+  {
+    id: 2,
+    name: "GitHub",
+    url: "https://github.com/nicolasperez19",
+  },
+  {
+    id: 3,
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/nicolás-pérez-5910711b2",
+  },
+];
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,26 +28,47 @@ const Header: React.FC = () => {
     setIsMenuOpen(!isMenuOpen);
   };
   return (
-    <header className="fixed top-0 bg-white w-full h-20 p-8 z-40">
-      <div className="w-full h-full m-auto px-5 md:px-24 flex justify-between items-center">
-        <Logo/>
-        <nav className="hidden md:flex space-x-4">
-          <CustomLink className='text-primary hover:underline hover:underline-offset-8' to="/projects">Projects</CustomLink>
-          <CustomLink className='text-primary hover:underline hover:underline-offset-8' to="https://github.com/nicolasperez19" target="_blank" rel="noopener noreferrer">Github</CustomLink>
-          <CustomLink className='text-primary hover:underline hover:underline-offset-8' to="https://www.linkedin.com/in/nicolás-pérez-5910711b2" target="_blank" rel="noopener noreferrer">LinkedIn</CustomLink>
+    <header className="fixed top-0 z-40 h-20 w-full bg-white p-8">
+      <div className="m-auto flex h-full w-full items-center justify-between px-5 md:px-24">
+        <Logo />
+        <nav className="hidden space-x-4 md:flex">
+          {links.map((link) => (
+            <CustomLink
+              key={link.id}
+              className="text-primary hover:underline hover:underline-offset-8"
+              to={link.url}
+            >
+              {link.name}
+            </CustomLink>
+          ))}
         </nav>
-        <button className="md:hidden" onClick={toggleMenu}>
+        <button
+          className="md:hidden"
+          aria-label="Open Menu"
+          onClick={toggleMenu}
+        >
           <List size={24} weight="bold" />
         </button>
       </div>
       {isMenuOpen && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 bg-white flex flex-col justify-center items-center text-left space-y-4 z-40">
-          <button className="absolute top-8 right-8" onClick={toggleMenu}>
+        <div className="fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col items-center justify-center space-y-4 bg-white text-left">
+          <button
+            className="absolute right-8 top-8"
+            aria-label="Close Menu"
+            onClick={toggleMenu}
+          >
             <X size={24} weight="bold" />
           </button>
-          <CustomLink className="text-2xl hover:underline hover:underline-offset-8" to="/projects" onClick={toggleMenu}>Projects</CustomLink>
-          <CustomLink className="text-2xl hover:underline hover:underline-offset-8" to="https://github.com/nicolasperez19" target="_blank" rel="noopener noreferrer" onClick={toggleMenu}>Github</CustomLink>
-          <CustomLink className="text-2xl hover:underline hover:underline-offset-8" to="https://www.linkedin.com/in/nicolás-pérez-5910711b2" target="_blank" rel="noopener noreferrer" onClick={toggleMenu}>LinkedIn</CustomLink>
+          {links.map((link) => (
+            <CustomLink
+              key={link.id}
+              className="text-2xl hover:underline hover:underline-offset-8"
+              to={link.url}
+              onClick={toggleMenu}
+            >
+              {link.name}
+            </CustomLink>
+          ))}
         </div>
       )}
     </header>
