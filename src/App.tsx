d69ca55295d362200a6c1870d0b7d4a0ad53b7d5
Nicolas Perez <nicolas_perez@brown.edu>
2024-05-11 13:16:38 -0400

    feat: Changed routing in App so that it shows not found  page if user goes to wrong path

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,92 @@
-import './App.css'
-import Header from './components/Header'
-import AboutMe from './components/AboutMe'
-import ProjectList from './components/ProjectList'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import ProjectPage from './components/ProjectPage'
-import Hero from './components/Hero'
-import Testimonials from './components/Testimonials'
-import Featured from './components/Featured'
-import BuyMeCoffee from './components/BuyMeCoffee'
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import Project from './pages/Project'
+import ScrollToTop from "./components/ScrollToTop";
+import Layout from "./layouts/Layout";
+import HomePage from "./pages/Home";
+// import { Suspense } from "react";
+import React from "react";
+import ProjectPage from "./pages/ProjectPage";
 
+// const LazyHome = React.lazy(() => import("./pages/Home"));
+// const LazyProject = React.lazy(() => import("./pages/Project"));
+
+/* TODO: TSDoc comments to components and pages for better documentation
+  @see {@link https://tsdoc.org/|TSDoc Documentation}
+*/
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="pt-20 bg-secondary text-primary">
-        <Header />
+    // <Router>
+    //   <div className="pt-20 bg-secondary text-primary">
+    //     <Header />
 
-        <main className='snap-mandatory snap-y overflow-scroll'>
-          <Routes>
-            <Route path="/cs1300-v2-portfolio" element={<>
-              <Hero title="Hi, I'm Nico!" subtitle="An award-winning software engineer" imageMobile="images/home/hero_image_mobile.png" imageDesktop="images/home/hero_image_desktop.png"/>
-              <Featured />
-              <AboutMe />
-              <ProjectList />
-              <Testimonials />
-              <Contact />
-              <BuyMeCoffee />
-            </>}>
-            </Route>
-            <Route path="/cs1300-v2-portfolio/projects/:slug" element={<ProjectPage />}>
-            </Route>
-          </Routes>
-        </main>
+    //     <main className='snap-mandatory snap-y overflow-scroll'>
+    //       <Routes>
+    //         <Route path="/cs1300-v2-portfolio" element={<>
+    //           <Hero title="Hi, I'm Nico!" subtitle="An award-winning software engineer" imageMobile="images/home/hero_image_mobile.png" imageDesktop="images/home/hero_image_desktop.png"/>
+    //           <Featured />
+    //           <AboutMe />
+    //           <ProjectList />
+    //           <Testimonials />
+    //           <Contact />
+    //           <BuyMeCoffee />
+    //         </>}>
+    //         </Route>
+    //         <Route path="/cs1300-v2-portfolio/projects/:slug" element={<ProjectPage />} />
+    //         <Route path="*" element={<>Not Found</>}/>
+    //       </Routes>
+    //     </main>
 
-        <Footer />
-      </div>
+    //     <Footer />
+    //   </div>
+    // </Router>
+
+    // Change base url
+    <Router basename={import.meta.env.BASE_URL}>
+      {/* <Router> */}
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route path="/cs1300-v2-portfolio/" element={<Layout />}> */}
+          <Route
+            index
+            element={
+              <HomePage />
+              // <Suspense
+              //   fallback={
+              //     <div>
+              //       <p>Loading...</p>
+              //     </div>
+              //   }
+              // >
+              //   <LazyHome />
+              // </Suspense>
+            }
+          />
+          <Route path="/projects">
+            {/* <Route path="/cs1300-v2-portfolio/projects/"> */}
+            <Route index element={<>Projects</>} />
+            <Route
+              path=":slug"
+              element={
+                <ProjectPage />
+                // <Suspense
+                //   fallback={
+                //     <div>
+                //       <p>Loading...</p>
+                //     </div>
+                //   }
+                // >
+                //   <LazyProject />
+                // </Suspense>
+              }
+            />
+          </Route>
+          <Route path="*" element={<>Not Found</>} />
+        </Route>
+      </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
