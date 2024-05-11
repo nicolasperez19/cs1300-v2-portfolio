bef5992607d69181ec87db8efd054a34e760616c
Nicolas Perez <nicolas_perez@brown.edu>
2024-05-11 13:21:47 -0400

    feat: Created Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,14 @@
 import CustomLink from "./CustomLink";
 
 const Logo: React.FC = () => {
-    return (
-        <CustomLink className="text-2xl font-display font-bold hover:underline hover:underline-offset-8" to="/cs1300-v2-portfolio">
-             Nico Perez.
-        </CustomLink>
-    );
-}
+  return (
+    <CustomLink
+      className="font-display text-2xl font-bold hover:underline hover:underline-offset-8"
+      to="/"
+    >
+      Nico Perez.
+    </CustomLink>
+  );
+};
 
 export default Logo;
